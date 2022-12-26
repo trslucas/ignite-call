9af749e69f03f54f2b02c6bdda95c03c6d42b545
Lucasm <trslucas@outlook.com>
2022-12-26 18:08:47 -0300

    First Commit/Home Layout

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+        <Text size={'xl'}>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          alt="Calendário simbolico da aplicação"
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
