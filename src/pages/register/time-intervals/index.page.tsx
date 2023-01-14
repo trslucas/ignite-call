7441445039d3e9e56dd3a299e96a25db10b02153
Lucasm <trslucas@outlook.com>
2023-01-13 23:23:22 -0300

    feat: Zod Transform and Validations on Intervals Form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -8,6 +8,7 @@ import {
 } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import {
+  FormError,
   IntervalBox,
   IntervalDay,
   IntervalInputs,
@@ -18,9 +19,29 @@ import { ArrowRight } from 'phosphor-react'
 import { z } from 'zod'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { getWeekDays } from '../../../utils/get-week-days'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export default function TimeIntervals() {
-  const timeIntervalsFormSchema = z.object({})
+  const timeIntervalsFormSchema = z.object({
+    intervals: z
+      .array(
+        z.object({
+          weekDay: z.number().min(0).max(6),
+          enabled: z.boolean(),
+          startTime: z.string(),
+          endTime: z.string(),
+        }),
+      )
+      .length(7)
+      .transform((intervals) =>
+        intervals.filter((interval) => interval.enabled),
+      )
+      .refine((intervals) => intervals.length > 0, {
+        message: 'Você precisa selecionar pelo menos um dia da semana!',
+      }),
+  })
+
+  type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
 
   const {
     register,
@@ -29,6 +50,7 @@ export default function TimeIntervals() {
     watch,
     formState: { isSubmitting, errors },
   } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -51,7 +73,9 @@ export default function TimeIntervals() {
 
   const weekDays = getWeekDays()
 
-  async function handleSetTimeInterval() {}
+  async function handleSetTimeInterval(data: TimeIntervalsFormData) {
+    console.log(data)
+  }
   return (
     <Container>
       <Header>
@@ -105,7 +129,11 @@ export default function TimeIntervals() {
             )
           })}
         </IntervalsContainer>
-        <Button type="submit">
+
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.message}</FormError>
+        )}
+        <Button type="submit" disabled={isSubmitting}>
           Próximo Passo
           <ArrowRight />
         </Button>
--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, Text, styled } from '@ignite-ui/react'
 
 export const IntervalBox = styled(Box, {
   marginTop: '$6',
@@ -41,4 +41,9 @@ export const IntervalInputs = styled('div', {
   'input::-webkit-calendar-picker-indicator': {
     filter: 'invert(100%) brightness(40%)',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$2',
 })
