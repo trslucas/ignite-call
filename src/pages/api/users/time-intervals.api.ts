7c06cd3bc46c283afb9ba439a1d686bdddd62e3a
Lucasm <trslucas@outlook.com>
2023-01-16 22:16:40 -0300

    fix: adjusting call on persistating datas

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -32,21 +32,21 @@ export default async function handler(
     return res.status(401).end()
   }
 
+  console.log(req.body)
   const { intervals } = timeIntervalsBodySchema.parse(req.body)
-  console.log(intervals)
 
-  // await Promise.all(
-  //   intervals.map((interval) => {
-  //     return prisma.userTimeInterval.create({
-  //       data: {
-  //         week_day: interval.weekDay,
-  //         time_start_in_minutes: interval.startTimeInMinutes,
-  //         time_end_in_minutes: interval.endTimeInMinutes,
-  //         user_id: session.user?.id,
-  //       },
-  //     })
-  //   }),
-  // )
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
 
   return res.status(201).end()
 }
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -23,50 +23,48 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '../../../utils/convert-time-string-to-minutes'
 import { api } from '../../../lib/axios'
 
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana!',
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término precisa ser pelo menos 1 hora distante do início',
+      },
+    ),
+})
+
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
+
 export default function TimeIntervals() {
-  const timeIntervalsFormSchema = z.object({
-    intervals: z
-      .array(
-        z.object({
-          weekDay: z.number().min(0).max(6),
-          enabled: z.boolean(),
-          startTime: z.string(),
-          endTime: z.string(),
-        }),
-      )
-      .length(7)
-      .transform((intervals) =>
-        intervals.filter((interval) => interval.enabled),
-      )
-      .refine((intervals) => intervals.length > 0, {
-        message: 'Você precisa selecionar pelo menos um dia da semana!',
-      })
-      .transform((intervals) => {
-        return intervals.map((interval) => {
-          return {
-            weekDay: interval.weekDay,
-            startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
-            endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
-          }
-        })
-      })
-      .refine(
-        (intervals) => {
-          return intervals.every(
-            (interval) =>
-              interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
-          )
-        },
-        {
-          message:
-            'O horário de término precisa ser pelo menos 1h depois do início',
-        },
-      ),
-  })
-
-  type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
-  type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
-
   const {
     register,
     control,
@@ -100,7 +98,9 @@ export default function TimeIntervals() {
   async function handleSetTimeInterval(data: any) {
     const { intervals } = data as TimeIntervalsFormOutput
 
-    await api.post('/users/time-intervals', intervals)
+    await api.post('/users/time-intervals', {
+      intervals,
+    })
   }
   return (
     <Container>
