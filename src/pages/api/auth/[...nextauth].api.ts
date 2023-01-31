81cd16e813dfde4f357af8fdf087d05b53c448b8
Lucasm <trslucas@outlook.com>
2023-01-31 14:59:26 -0300

    Feat: Refresh token from Google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -17,6 +17,9 @@ export function buildNextAuthOptions(
         // permissões
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
           },
